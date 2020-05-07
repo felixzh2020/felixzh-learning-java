1733a9804de907d10bbe6daead8e0d0c19d15a0b
felixzh <felixzh>
2020-05-07 10:11:51 +0800

    增加Enum案例

---
+++ src/main/java/Enum/EnumTest.java
@@ -0,0 +1,19 @@
+package Enum;
+
+public class EnumTest {
+    public static void main(String[] args) {
+        System.out.println(Status.values().length);
+        System.out.println(Status.success);
+        System.out.println(Status.fail);
+        System.out.println(Status.valueOf("fail"));
+        try {
+            System.out.println(Status.valueOf("fails"));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        for (Status status : Status.values()) {
+            System.out.println("==" + status);
+        }
+    }
+}
