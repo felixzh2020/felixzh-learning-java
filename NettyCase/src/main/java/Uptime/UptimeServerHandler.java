486c6dd7749757f7714612abc25aee81ed0df844
felixzh <felixzh>
2020-12-30 15:17:31 +0800

    NettyCase: Uptime

---
+++ NettyCase/src/main/java/Uptime/UptimeServerHandler.java
@@ -0,0 +1,20 @@
+package Uptime;
+
+import io.netty.channel.ChannelHandler.Sharable;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+@Sharable
+public class UptimeServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // discard
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
