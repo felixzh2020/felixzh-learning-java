7d92ac10dcb1027342cd3dfa48ccb92be8b18323
felixzh <felixzh>
2020-12-30 16:09:36 +0800

    NettyCase: Telnet

---
+++ NettyCase/src/main/java/Telnet/TelnetClient.java
@@ -0,0 +1,76 @@
+package Telnet;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Simplistic telnet client.
+ * <p>
+ * 可以用于连接 nc -l port 启动的服务
+ */
+public final class TelnetClient {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8992" : "8023"));
+
+    public static void main(String[] args) throws Exception {
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (SSL) {
+            sslCtx = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        } else {
+            sslCtx = null;
+        }
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new TelnetClientInitializer(sslCtx));
+
+            // Start the connection attempt.
+            Channel ch = b.connect(HOST, PORT).sync().channel();
+
+            // Read commands from the stdin.
+            ChannelFuture lastWriteFuture = null;
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            for (; ; ) {
+                String line = in.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                // Sends the received line to the server.
+                lastWriteFuture = ch.writeAndFlush(line + "\r\n");
+
+                // If user typed the 'bye' command, wait until the server closes
+                // the connection.
+                if ("bye".equals(line.toLowerCase())) {
+                    ch.closeFuture().sync();
+                    break;
+                }
+            }
+
+            // Wait until all messages are flushed before closing the channel.
+            if (lastWriteFuture != null) {
+                lastWriteFuture.sync();
+            }
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
+
