e2f208e0fa6ee4016e7ac53586709c43fdc89042
felixzh <felixzh>
2020-12-30 11:09:34 +0800

    NettyCase: Discard

---
+++ NettyCase/src/main/java/Discard/DiscardServerHandler.java
@@ -0,0 +1,24 @@
+package Discard;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+import java.util.Date;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // discard, only log
+        System.out.println(new Date() + ": " + msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
