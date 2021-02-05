18b03b1e65ddc65f351cab148affe2f51e3f5a07
felixzh <felixzh>
2021-02-05 09:42:02 +0800

    TextProtocols.QuoteOfTheMoment

---
+++ NettyCase/src/main/java/TextProtocols/QuoteOfTheMoment/QuoteOfTheMomentClientHandler.java
@@ -0,0 +1,24 @@
+package TextProtocols.QuoteOfTheMoment;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.util.CharsetUtil;
+
+public class QuoteOfTheMomentClientHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, DatagramPacket msg) throws Exception {
+        String response = msg.content().toString(CharsetUtil.UTF_8);
+        if (response.startsWith("QOTM: ")) {
+            System.out.println("Quote of the Moment: " + response.substring(6));
+            ctx.close();
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
---
+++ NettyCase/src/main/java/TextProtocols/QuoteOfTheMoment/QuoteOfTheMomentServer.java
@@ -0,0 +1,33 @@
+package TextProtocols.QuoteOfTheMoment;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+/**
+ * A UDP server that responds to the QOTM (quote of the moment) request to a {@link QuoteOfTheMomentClient}.
+ * <p>
+ * Inspired by <a href="https://docs.oracle.com/javase/tutorial/networking/datagrams/clientServer.html">the official
+ * Java tutorial</a>.
+ */
+public final class QuoteOfTheMomentServer {
+
+    private static final int PORT = Integer.parseInt(System.getProperty("port", "7686"));
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioDatagramChannel.class)
+                    .option(ChannelOption.SO_BROADCAST, true)
+                    .handler(new QuoteOfTheMomentServerHandler());
+
+            b.bind(PORT).sync().channel().closeFuture().await();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
